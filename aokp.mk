25f9d27b74e33c46526065bea69ed676f75c2d99
Jacob Bell <jacob@fzservers.com>
2012-03-02 15:58:41 -0500

    Set buildid

--- aokp.mk
+++ aokp.mk
@@ -37,6 +37,9 @@ $(call inherit-product, vendor/aokp/configs/common_tablet.mk)
 -include vendor/aokp/products/common_versions.mk
 
 # Set those variables here to overwrite the inherited values.
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_ID=IML74K
+
 PRODUCT_NAME := aokp_a500
 PRODUCT_DEVICE := a500
 PRODUCT_BRAND := Acer
